5c3736f01163726e682db26570afc429d88e8f18
vsss <vsss@mac.ssw.umb.edu>
2025-04-24 19:54:06 -0400

    Restored entire src/ directory from commit e0744e3

---
+++ Homework_06/src/main/umbcs680/FileSystem.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class FileSystem{
+
+    private static FileSystem instance = null;
+
+    private LinkedList <Directory> rootDirs = new LinkedList<>();
+
+    private FileSystem(){} //private constructor to avoid instantitation from outside.
+
+    public FileSystem getFileSystem(){
+        if (instance == null){
+            instance = new FileSystem();
+        }
+        return instance;
+    }
+    public LinkedList<Directory> getRootDirs() {
+        return rootDirs;
+    }
+    public void appendRootDir(Directory root){
+         rootDirs.add(root);
+    }
+}
