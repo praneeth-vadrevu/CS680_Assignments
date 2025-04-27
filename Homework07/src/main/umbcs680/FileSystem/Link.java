f5456d52da5685843326a10540e040d91fbc5cf1
vsss <vsss@MacBook-Air-3.local>
2025-04-27 14:33:56 -0400

    Implemented the Link.java class along with the rest of the FileSystem Codebase

---
+++ Homework07/src/main/umbcs680/FileSystem/Link.java
@@ -0,0 +1,29 @@
+import java.time.LocalDateTime;
+
+public class Link extends FSElement {
+    private FSElement target;
+
+    public Link(Directory parent, String name, int size, LocalDateTime creationTime, FSElement target) {
+        super(parent, name, size, creationTime);
+        this.target = target;
+    }
+
+    public FSElement getTarget() {
+        return target;
+    }
+
+    @Override
+    public boolean isDirectory() {
+        return target.isDirectory();
+    }
+
+    @Override
+    public boolean isLink() {
+        return true;
+    }
+
+    @Override
+    public int getSize() {
+        return target.getSize();
+    }
+}
